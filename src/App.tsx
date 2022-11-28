63a025c72dee1a6ccdc3d7125101996c29a704bb
Francisco Moncada B <fmoncadabuelvas0112@gmail.com>
2022-11-27 22:55:33 -0500

    modified legacyStore

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,2 @@
 import React from 'react';
-import './App.css';
-import Greeting from './components/Greeting';
 
-
-function App() {
-  return (
-    <Greeting
-      name="typescript"
-      enthusiasmLevel={10}
-    />
-  );
-}
-
-export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { legacy_createStore } from 'redux';
+import { enthusiasm }  from './reducers/index';
+import { StoreState }  from './types/index'; 
+import Greeting from './containers/Greeting';
+import { Provider } from 'react-redux';
+
+const store = legacy_createStore<StoreState>({
+  languageName: 'TypeScript',
+  enthusiasmLevel: 1,
+}, enthusiasm);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <Greeting />
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
